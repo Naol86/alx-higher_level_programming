4aa844d42a78e2d7376a7e0ba3efab8bd66dcdd1
naol86 <lijnaolkasinet@gmail.com>
2023-10-04 11:45:57 +0300

    test

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i != 0:
-        print(",", end=" ")
-    print("{:02}".format(i), end='')
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
