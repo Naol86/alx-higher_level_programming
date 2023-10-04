a9cb0b65760241cdf682055c57279158e6d9aa44
naol86 <lijnaolkasinet@gmail.com>
2023-10-04 16:36:42 +0300

    test

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 for i in range(1, 100):
-    if i < 10:
-        print("{:02}".format(i), end=', ')
-        continue
     if ((i % 10) * 10 + i // 10) > i:
-        print(i, end='')
+        print("{:02}".format(i), end='')
         if i < 89:
             print(", ", end='')
 print(' ')
