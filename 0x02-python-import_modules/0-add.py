b9d8a6cf778856ee521a58d347c78e025ba18872
naol86 <lijnaolkasinet@gmail.com>
2023-10-06 15:55:46 +0300

    test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-__import__ add_0
-a = 1
-b = 2
-print("{}".format(add_0.add(a,b)))
 
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a + b)))
