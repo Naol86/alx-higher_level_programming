3cd94c1fb43dbe6933f598806c410f578a6c7107
naol86 <lijnaolkasinet@gmail.com>
2023-10-24 16:01:10 +0300

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""oop learning day 1"""
 
 
 class Square:
-
+    """this is a square class"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""oop learning day 1"""
+
 
 class Square:
+    """this is a square class"""
 
     def __init__(self, size=0):
         if type(size) != int:
