417eea1cf3df3ff286c6d2165b5b45acb251268d
naol86 <lijnaolkasinet@gmail.com>
2023-10-24 13:09:44 +0300

    test 1

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+class Square:
+
+    def __init__(self, size=0, position=(0, 0)):
+        if type(position) != tuple or (position[0] < 0 or position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__position = position
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value=None):
+        if value is not None:
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple or (value[0] > 0 or value[1]):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+this oop in python 
