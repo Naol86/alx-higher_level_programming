86a7b301c943601118cfda3aa02cf15cd8911900
naol86 <lijnaolkasinet@gmail.com>
2023-10-24 15:56:47 +0300

    test

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""oop learning day 1"""
+
 
 class Square:
-
+    """this is Square class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""oop learning day 1"""
+
 
 class Square:
     """this is a square class"""
