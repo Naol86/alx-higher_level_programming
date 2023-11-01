3efa8fd2b5c83ddf5d7cdac8ab604fb07e5cd109
naol86 <lijnaolkasinet@gmail.com>
2023-11-01 21:38:21 +0300

    test

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -52,10 +52,13 @@ def sol(n):
 
 def main():
     if __name__ == "__main__":
-        if not sys.argv[1].isdigit():
-            print("N must be a Number")
+        if len(sys.argv) != 2:
+            print("Usage: nqueens N")
             sys.exit(1)
-        elif int(sys.argv[1]) < 4:
+        if sys.argv[1].isdigit() is False:
+            print("N must be a number")
+            sys.exit(1)
+        if int(sys.argv[1]) < 4:
             print("N must be at least 4")
             sys.exit(1)
         a = int(sys.argv[1])
