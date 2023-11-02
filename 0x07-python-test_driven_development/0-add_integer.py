df10a529266539c893e3470d5633701dcb3918c2
naol86 <lijnaolkasinet@gmail.com>
2023-11-02 20:40:56 +0300

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,7 +13,7 @@ def add_integer(a, b=98):
     if type(a) != float and type(a) != int:
         raise TypeError("a must be an integer")
     elif type(b) != float and type(b) != int:
-        raise TypeError("a must be an integer")
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
