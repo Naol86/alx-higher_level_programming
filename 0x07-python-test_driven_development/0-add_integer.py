38e88eb6395babe1679a2ea36d99bca592fd4236
naol86 <lijnaolkasinet@gmail.com>
2023-11-02 20:24:27 +0300

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,11 +2,14 @@
 """
 This is the "0-add_integer" module.
 The 0-add_integer module supplies one function, add_integer(a, b).
+let's do it
 """
 
 
 def add_integer(a, b=98):
-    """this is add integer function to test doctest"""
+    """
+        this is add integer function to test doctest
+    """
     if type(a) != float and type(a) != int:
         raise TypeError("a must be an integer")
     elif type(b) != float and type(b) != int:
