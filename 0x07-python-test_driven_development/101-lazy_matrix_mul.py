dae27379b97bc60f7aa7b0dd0d89f8ef1d08dd3d
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 17:15:45 +0300

    test 101

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+matrix using numpy
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """matrices multiplication using numpy"""
+    return np.matmul(m_a, m_b)
