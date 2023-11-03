9a266308bea026e7c4edab56c19795b8bd20530f
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 08:42:08 +0300

    test

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+this is unit test for max integer function
+"""
+import unittest
+
+max_integer = __import__("6-max_integer").max_integer
+
+class TestMaxInteger(unittest.TestCase):
+	"""unit test cases"""
+	def module_docstring(self):
+		m = __import__('6-max_integer').__doc__
+		self.assertTrue(len(m) > 1)
+	
+	def function_docstring(self):
+		f = __import__("6-max_integer").max_integer.__doc__
+		self.assertTrue(len(f) > 1)
+	
+	def empty_list(self):
+		self.assertIsNone(max_integer([]))
+
+if __name__ == "__main__":
+	unittest.main()
