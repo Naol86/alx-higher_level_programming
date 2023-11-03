5e54cabb243ef0cc6f84091e9aa6ae24765bab11
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 09:11:39 +0300

    test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,21 +5,30 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-	"""unit test cases"""
-	def test_module_docstring(self):
-		m = __import__('6-max_integer').__doc__
-		self.assertTrue(len(m) > 1)
-	
-	def test_function_docstring(self):
-		f = __import__("6-max_integer").max_integer.__doc__
-		self.assertTrue(len(f) > 1)
-	
-	def test_empty_list(self):
-		self.assertIsNone(max_integer([]))
-	
-	def test_negative_list(self):
-		lis = [-12, -23, -24, -56]
-		self.assertEqual(max_integer(lis), -12)
+    """unit test cases"""
+    def test_module_docstring(self):
+        m = __import__('6-max_integer').__doc__
+        self.assertTrue(len(m) > 1)
+
+    def test_function_docstring(self):
+        f = __import__("6-max_integer").max_integer.__doc__
+        self.assertTrue(len(f) > 1)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]))
+
+    def test_negative_list(self):
+        lis = [-12, -23, -24, -56]
+        self.assertEqual(max_integer(lis), -12)
+
+    def test_pos_neg(self):
+        lis = [1, 2, 3, 4, -12, -45]
+        self.assertEqual(max_integer(lis), 4)
+
+    def test_invalid_input(self):
+        lis = ["naol", 12, 34, "kasinet"]
+        with self.assertRaises(TypeError):
+            max_integer(lis)
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
