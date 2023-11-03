fdfba2659a9dd5409bdf6f58bfefedaf4c186873
naol86 <lijnaolkasinet@gmail.com>
2023-11-03 09:05:22 +0300

    test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,16 +6,20 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 	"""unit test cases"""
-	def module_docstring(self):
+	def test_module_docstring(self):
 		m = __import__('6-max_integer').__doc__
 		self.assertTrue(len(m) > 1)
 	
-	def function_docstring(self):
+	def test_function_docstring(self):
 		f = __import__("6-max_integer").max_integer.__doc__
 		self.assertTrue(len(f) > 1)
 	
-	def empty_list(self):
+	def test_empty_list(self):
 		self.assertIsNone(max_integer([]))
+	
+	def test_negative_list(self):
+		lis = [-12, -23, -24, -56]
+		self.assertEqual(max_integer(lis), -12)
 
 if __name__ == "__main__":
 	unittest.main()
