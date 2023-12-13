884caa7ebb0742911b19ee9be54409c0bdc7de2e
naol86 <lijnaolkasinet@gmail.com>
2023-12-13 17:53:18 +0300

    test

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create database called hbtn_0d_usa and table called states in that database
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(
-	`id` INT NOT NULL AUTOINCREMENT,
+	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(256) NOT NULL,
 	PRIMARY KEY(`id`)
 );
