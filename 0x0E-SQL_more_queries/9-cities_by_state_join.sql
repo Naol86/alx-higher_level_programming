0305b86b656c9d76c2c8ae65be6c390b78b67b8b
naol86 <lijnaolkasinet@gmail.com>
2023-12-13 22:47:21 +0300

    teset

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- listing cities with thier state
 SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
 FROM `cities`
-JOIN INNER `states` ON `cities`.`state_id` = `states`.`id`
+INNER JOIN `states` ON `cities`.`state_id` = `states`.`id`
 ORDER BY `cities`.`id`;
