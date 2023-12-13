92180343d04c14140cf062bf2f6fe2c9bbbf485c
naol86 <lijnaolkasinet@gmail.com>
2023-12-13 22:43:46 +0300

    test

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,1 +1,6 @@
--- 
+-- list where tv show genres id is null
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `tv_shows` LEFT JOIN `tv_show_genres`
+ON `tv_shows`.`id`  = `tv_show_genres`.`show_id`
+WHERE `tv_show_genres`.`genre_id` IS NULL
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,5 @@
--- 
+-- listing cities with thier state
+SELECT `cities`.`id`, `cities`.`cities_name`, `states`.`name`
+FROM `cities`
+JOIN INNER `states` ON `cities`.`state_id` = `states`.`id`
+ORDER BY `cities`.`id`;
