167ab2b4b761d0b3ee9f48b5a2c82968f45f55b8
naol86 <lijnaolkasinet@gmail.com>
2023-12-13 21:22:59 +0300

    test

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- select all cities found in california
 SELECT `id`,`name` FROM `cities`
-WHERE `state_id` = (SELECT `id` FROM 'states' WHERE `name` = "California")
+WHERE `state_id` IN (SELECT `id` FROM 'states' WHERE `name` = "California")
 ORDER BY `id`;
