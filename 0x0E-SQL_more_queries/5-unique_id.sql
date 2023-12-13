e8315cb9c0da6e46b6e198ade640c5f9d9fb9300
naol86 <lijnaolkasinet@gmail.com>
2023-12-13 17:59:30 +0300

    test

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,5 @@
 -- create new table called uniqe_id where id must be uniqe
 CREATE TABLE IF NOT EXISTS `uniqe_id` (
-	`id` INT DEFAULT 1 UNIQUE,
+	`id` INT 	DEFAULT 1 UNIQUE,
 	`name` VARCHAR(256)
 );
