9a5334f2e5719076dfb88e606c0fac4c5959fefb
naol86 <lijnaolkasinet@gmail.com>
2023-12-14 22:08:05 +0300

    test

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,7 @@
 -- list all tv genres name that exist in tv show genres
-SELECT tv_genres.`name` FROM tv_genres INNER JOIN tv_show_genres
-WHERE tv_show_genres.show_id = 8 AND tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_genres.`name`
+SELECT `name` FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY name
+ORDER BY name ASC;
