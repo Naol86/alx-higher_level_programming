f0dd3c8483133b8072c5f1be008f8656994d28e7
naol86 <lijnaolkasinet@gmail.com>
2023-12-14 21:53:25 +0300

    test

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
 -- list all tv genres name that exist in tv show genres
-SELECT tv_genres.`name` FROM tv_genres INNER JOIN tv_show_genres
-WHERE tv_show_genres.show_id = 8 AND tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_genres.`name`;
+SELECT `tv_genres`.`name` FROM `tv_genres` INNER JOIN `tv_show_genres`
+WHERE `tv_show_genres`.`show_id` = 8 AND `tv_genres`.`id` = `tv_show_genres`.`genre_id`
+ORDER BY `tv_genres`.`name`;
