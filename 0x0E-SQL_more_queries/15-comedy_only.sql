b57b7c911f4d7d7b39eee7b0be1cdfbb454d1258
naol86 <lijnaolkasinet@gmail.com>
2023-12-14 22:18:35 +0300

    test

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,1 +1,4 @@
--- 
+-- select all comedy in tv shows
+SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = 5
+ORDER BY tv_shows.title;
