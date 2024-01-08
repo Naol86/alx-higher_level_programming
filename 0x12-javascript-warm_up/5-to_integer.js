2770fe260da5bfad43d64747e6bf51fb754c81bf
naol86 <lijnaolkasinet@gmail.com>
2024-01-08 21:43:11 +0300

    test

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const argv = process.argv;
-
 const num = parseInt(argv[2]);
 
-if (isNaN(num)) {
+if (isNaN(argv[2])) {
   console.log('Not a number');
 } else {
   console.log('My number is: ' + num);
