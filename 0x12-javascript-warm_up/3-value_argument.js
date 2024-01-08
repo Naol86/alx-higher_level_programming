79844d0d5e25d771c2ad7811353b7f230fbd51ff
naol86 <lijnaolkasinet@gmail.com>
2024-01-08 21:40:02 +0300

    test

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 const argv = process.argv;
 
-if (argv.length === 2) {
+if (argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(argv[2]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,5 +7,5 @@ const num = parseInt(argv[2]);
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log(`My number is: ${num}`);
+  console.log('My number is: ' + num);
 }
