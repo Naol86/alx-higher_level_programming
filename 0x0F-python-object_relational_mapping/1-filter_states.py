5a8a301cbf49cfd64d0657ae81877c4636a7db9c
naol86 <lijnaolkasinet@gmail.com>
2024-01-15 21:45:01 +0300

    test

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,2 @@
 #!/usr/bin/python3
 
-import sys
-import MySQLdb
-
-if __name__ == '__main__':
-	argv = sys.argv
-	db = MySQLdb.connect(user = argv[1], password = argv[2], db = argv[3])
-	temp = db.cursor()
-	temp.execute("SELECT * FROM `states`")
-	for i in tempprint(i)
