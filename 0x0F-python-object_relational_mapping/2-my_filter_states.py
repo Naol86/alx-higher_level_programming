22f242792bc95d1cf6c79e2ac4d6cb67b4239bb0
naol86 <lijnaolkasinet@gmail.com>
2024-01-15 22:24:23 +0300

    test

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,11 @@ if __name__ == '__main__':
     argv = sys.argv
     db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
     temp = db.cursor()
-    temp.execute(f"SELECT * FROM `states` WHERE `name` = \"{argv[4]}\"")
+    temp.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
     states = temp.fetchall()
     for state in states:
         print(state)
+    temp.close
+    db.close()
 
--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    argv = sys.argv
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], password=argv[2], db=argv[3])
+    temp = db.cursor()
+    temp.execute("SELECT * FROM states WHERE name LIKE %s",(sys.argv[4],))
+    states = temp.fetchall()
+    for state in states:
+        print(state)
