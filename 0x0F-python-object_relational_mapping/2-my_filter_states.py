c8e361767cf2b70ab63035a1b672c0070b8b3b25
naol86 <lijnaolkasinet@gmail.com>
2024-01-19 13:19:38 +0300

    test

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-
 """
-    check if user input is exist in our database
-    but with out check it it can destroy out db
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
 """
-
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    argv = sys.argv
-    db = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3])
-    temp = db.cursor()
-    temp.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                        passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
     id ASC".format(sys.argv[4]))
-    states = temp.fetchall()
+    states = c.fetchall()
     for state in states:
-        print(state)
-    temp.close
+        if state[1] == sys.argv[4]:
+            print(state)
+    c.close()
     db.close()
-
